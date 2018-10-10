30f42c4c60c8fae49bd85969efc37b4d0fd86f9c
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-10-10 23:15:33 +0200

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+const tilebelt = require('@mapbox/tilebelt')
+
+if (process.argv.length !== 3) {
+  console.log(`usage: node index.js {Z} > config.json`)
+}
+
+const z = parseInt(process.argv[2])
+let config = { extracts: [] }
+
+for (let x = 0; x < 2 ** z; x++) {
+  for (let y  = 0; y < 2 ** z; y++) {
+     config.extracts.push({
+       output: `${z}-${x}-${y}.osm.pbf`,
+       output_format: 'pbf',
+       bbox: tilebelt.tileToBBOX([x, y, z])
+     })
+  }
+}
+
+console.log(JSON.stringify(config, null, 2))
